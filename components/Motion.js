e9a84960d81c028963d6b8a1b5906005cc305abd
amitrawat <amitrawat4532@gmail.com>
2022-11-28 17:07:29 +0530

    text

--- components/Motion.js
+++ components/Motion.js
@@ -34,11 +34,11 @@ export default function Motion() {
         animate={{ opacity: 1 }}
         id="motion_container"
       >
-        <motion.div className="w-96 h-full bg-white border-2 border-black">
-          <h1>
-            Hey this is me amit rawat and im from dehradun uttarakhand india
-          </h1>
-          <p>my home town is suituated in rudraprayag india</p>
+        <motion.div className="w-96 h-96 bg-white border-2 border-black">
+          <h1>Hey this is dummy text1</h1>
+          <h2>Hey this is dummy text2</h2>
+
+          <h3>Hey this is dummy text3</h3>
         </motion.div>
       </motion.div>
     </>
