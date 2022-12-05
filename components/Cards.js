2e9eb840fc7d5991f46868f644e14e6d4c36cbf0
Muzammil-khan710 <khanmuzammil549@gmail.com>
2022-12-05 22:30:46 +0530

    feat/method using scroll

--- components/Cards.js
+++ components/Cards.js
@@ -1,20 +1,23 @@
 import React from "react";
-// import { useScroll } from "framer-motion";
-import { motion, Variants } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-function Cards({ scrollY }) {
-  // const { scrollY, scrollYProgress } = useScroll();
+function Cards() {
+  const { scrollY } = useScroll();
+
+  const x = scrollY
+  const newVal = useTransform(x, [1000,2000], [-45, 45 ])
+  const filterVal = Math.floor(Number(newVal.current))
+
+
   const cardVariants = {
     offscreen: {
       y: 0,
     },
     onscreen: {
-      y: -50,
+      y: filterVal,
 
       transition: {
-        // type: "spring",
-        // bounce: 0.4,
-        duration: 0.5,
+        duration: 2
       },
     },
   };
@@ -40,41 +43,31 @@ function Cards({ scrollY }) {
     },
   ];
   return (
-    <>
-      <motion.div className="w-full h-[170vh] bg-slate-200  flex flex-row justify-center items-end overflow-hidden">
-        {Cards.map((el, id) => {
-          return (
-            <>
-              <div className="flex  flex-wrap py-20" key={id}>
-                <motion.div className="h-[110vh] w-[250px] flex flex-col justify-center items-center">
-                  {el?.img.map((x, key) => (
-                    <motion.div
-                      whileHover={{ scale: 1.1 }}
-                      transition={{
-                        type: "spring",
-                        stiffness: 400,
-                        damping: 20,
-                      }}
+    <motion.div className="w-full h-[170vh] bg-slate-200  flex flex-row justify-center items-end overflow-hidden">
+      {Cards.map((el, id) => {
+        return (
+            <div className="flex  flex-wrap py-20" key={id}>
+              <motion.div className="h-[110vh] w-[250px] flex flex-col justify-center items-center">
+                {el?.img.map((x, key) => (
+                  <motion.div
+                    key={key}
+                    initial="offscreen"
+                    whileInView="onscreen"
+                    viewport={{ once: false, amount: "all" }}
+                    variants={cardVariants}
+                  >
+                    <motion.img
                       key={key}
-                      initial="offscreen"
-                      whileInView="onscreen"
-                      viewport={{ once: false, amount: "all" }}
-                      variants={cardVariants}
-                    >
-                      <motion.img
-                        key={key}
-                        src={x}
-                        className="h-[500px] w-[250px]"
-                      />
-                    </motion.div>
-                  ))}
-                </motion.div>
-              </div>
-            </>
-          );
-        })}
-      </motion.div>
-    </>
+                      src={x}
+                      className="h-[500px] w-[250px]"
+                    />
+                  </motion.div>
+                ))}
+              </motion.div>
+            </div>
+        );
+      })}
+    </motion.div>
   );
 }
 
