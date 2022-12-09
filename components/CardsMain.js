958434c52c63ff8702fe962253bfd1f5b181dc75
amitrawat <amitrawat4532@gmail.com>
2022-12-09 15:08:59 +0530

    added animation

--- components/CardsMain.js
+++ components/CardsMain.js
@@ -3,39 +3,52 @@
 import { motion } from "framer-motion";
 
 const CardsMain = () => {
-  // const cardVariants = {
-  //   offscreen: {
-  //     y: 0,
-  //   },
-  //   onscreen: {
-  //     y: 60,
-  //     transition: {
-  //       // type: "spring",
-  //       // bounce: 0.4,
-  //       duration: 1,
-  //     },
-  //   },
-  // };
-  // const cardVariantsDown = {
-  //   offscreen: {
-  //     y: 0,
-  //   },
-  //   onscreen: {
-  //     y: 50,
-  //     transition: {
-  //       // type: "spring",
-  //       // bounce: 0.4,
-  //       duration: 4,
-  //     },
-  //   },
-  // };
+  const cardVariants = {
+    offscreen: {
+      y: 0,
+    },
+    onscreen: {
+      y: -100,
+      transition: {
+        // type: "spring",
+        // bounce: 0.4,
+        duration: 4,
+      },
+    },
+  };
+  const cardVariantsDown = {
+    offscreen: {
+      y: 0,
+    },
+    onscreen: {
+      y: 70,
+      transition: {
+        // type: "spring",
+        // bounce: 0.4,
+        duration: 4,
+      },
+    },
+  };
+  const cardVariantsDowntwo = {
+    offscreen: {
+      y: 20,
+    },
+    onscreen: {
+      y: 180,
+      transition: {
+        // type: "spring",
+        // bounce: 0.4,
+        duration: 4,
+      },
+    },
+  };
   return (
-    <div className="flex h-full w-[110vw] pt-52  justify-center items-center bg-#ebeef4 relative left-[-4vw] overflow-hidden gap-6">
+    <div className="flex h-full w-[110vw] pt-52  justify-center items-center bg-#ebeef4 relative left-[-6vw] overflow-hidden gap-6">
       {/* Card Coantainer 1 */}
       <motion.div>
         <motion.img
           // animate={{ y: -500 }}
-          className="two "
+          className="two h-[460px] w-[220px]"
           src="../images/card4.png"
           alt="image"
         />
@@ -44,7 +57,7 @@ const CardsMain = () => {
       <div className="heyyy  ">
         <div className="h-two py-3">
           <motion.img
-            className="two "
+            className="two  h-[460px] w-[220px]"
             src="../images/card5.png"
             alt="image"
             // initial="offscreen"
@@ -56,7 +69,7 @@ const CardsMain = () => {
 
         <div className="h-three py-3">
           <motion.img
-            className="two "
+            className="two h-[460px] w-[220px]"
             src="../images/card3.png"
             alt="image"
             // initial="offscreen"
@@ -71,32 +84,32 @@ const CardsMain = () => {
       <div className="h-four ">
         <motion.img
           animate={{ y: -50 }}
-          className="two "
+          className="two  h-[460px] w-[220px]"
           src="../images/card1.png"
           alt="image"
-          // initial="offscreen"
-          // whileInView="onscreen"
-          // viewport={{ once: false, amount: "all" }}
-          // variants={cardVariants}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: false, amount: "all" }}
+          variants={cardVariantsDown}
         />
       </div>
       {/* Card Coantainer 4 */}
       <div className="h-five ">
         <motion.img
-          className="two "
+          className="two h-[460px] w-[220px]"
           src="../images/card9.png"
           alt="image"
-          // initial="offscreen"
-          // whileInView="onscreen"
-          // viewport={{ once: false, amount: "all" }}
-          // variants={cardVariants}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: false, amount: "all" }}
+          variants={cardVariantsDowntwo}
         />
       </div>
       {/* Card Coantainer 5 */}
       <div className="hey translate-y-24">
         <div className="h-six py-3">
           <motion.img
-            className="two "
+            className="two h-[460px] w-[220px] "
             src="../images/card7.png"
             alt="image"
             // initial="offscreen"
@@ -106,8 +119,12 @@ const CardsMain = () => {
           />
         </div>
 
-        <div className="h-six py-3">
-          <motion.img className="two " src="../images/card10.png" alt="image" />
+        <div className="h-six py-3 ">
+          <motion.img
+            className="two h-[460px] w-[220px]"
+            src="../images/card10.png"
+            alt="image"
+          />
         </div>
       </div>
 
@@ -115,20 +132,20 @@ const CardsMain = () => {
       <div className="h-seven ">
         <motion.img
           animate={{ y: -40 }}
-          className="two "
-          src="../images/card10.png"
+          className="two h-[460px] w-[220px] "
+          src="../images/card6.png"
           alt="image"
-          // initial="offscreen"
-          // whileInView="onscreen"
-          // viewport={{ once: false, amount: "all" }}
-          // variants={cardVariants}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: false, amount: "all" }}
+          variants={cardVariants}
         />
       </div>
       {/* Card Coantainer 7 */}
       <div className="">
         <motion.img
-          animate={{ y: -120 }}
-          className="two "
+          animate={{ y: -240 }}
+          className="two h-[460px] w-[220px] "
           src="../images/card8.png"
           alt="image"
           // initial="offscreen"
